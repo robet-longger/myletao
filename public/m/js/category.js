a68fe16fde3f45e1542def3ce3096f0bfcdc260b
huangfeilong <1390900070@qq.com>
2017-11-02 22:17:43 +0800

    change something

--- public/m/js/category.js
+++ public/m/js/category.js
@@ -1,3 +1,7 @@
+mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});  
+
 
 $(function () {
     // 一级目录
@@ -46,7 +50,6 @@ var getSecondCategory = function (pa, callback) {
         data: pa,
         success: function (res) {
             callback && callback(res);
-
         }
     })
 }
